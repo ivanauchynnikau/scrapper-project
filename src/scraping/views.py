0f0cb090b856302fc853811237dc4934c0ed94fc
Ivan Auchynnikau <ivan.auchynnikau@itechart-group.com>
2020-06-03 01:21:22 +0300

    Add pagination for vacancies list

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .forms import FindForm
 from .models import Vacancy
@@ -5,15 +6,32 @@ from .models import Vacancy
 
 def home_view(request):
     form = FindForm()
+
+    return render(request, 'scraping/home.html', {'form': form})
+
+
+def list_view(request):
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    _filter = {}
+    page_obj = []
+
+    context = {'city': city, 'language': language, 'form': form}
 
     if city or language:
+        _filter = {}
+
         if city:
             _filter['city__slug'] = city
         elif language:
             _filter['language__slug'] = language
 
-    qs = Vacancy.objects.filter(**_filter)  # TODO зачем тут **_filter ???
-    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
+        qs = Vacancy.objects.filter(**_filter)  # TODO зачем тут **_filter ???
+
+        paginator = Paginator(qs, 10)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['object_list'] = page_obj
+
+    return render(request, 'scraping/list.html', context)
